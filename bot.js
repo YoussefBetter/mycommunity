6c21f687fc2327661e6a584d7ca65b3254f38529
YoussefBetter <47484666+YoussefBetter@users.noreply.github.com>
2019-03-14 20:48:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,17 @@ const client = new Discord.Client();
 const prefix = "8";
 
 client.on('ready', () => { // لما يشتغل
+  
   client.user.setGame("YB's Community", "https://www.twitch.tv/alpha");
   console.log(`Logged in as [ ${client.user.tag}! ]`);
   console.log('[           BOT IS ONLINE         ]')
 });
+
+client.on('ready', async => {
+    setInterval(() => {
+        client.channels.find('id', '555836400491298817').setName("");
+    })
+}), 5000;
 
 client.on('message', message => { // اقتراح
     if (message.content.startsWith(prefix + 'sug')) {
@@ -348,24 +355,6 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 client.on("guildMemberAdd", member => {
   member.addRole(member.guild.roles.find("name","• Guest"));
-});
-
-client.on('message',async message => {
-  if(message.content.startsWith(prefix + "setrex")) {
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
-  message.channel.send('✅| **تم عمل الروم بنجاح**');
-  message.guild.createChannel(`Members Count : [ ${message.guild.members.size} ]` , 'voice').then(c => {
-    console.log(`Count Members channel setup for guild: \n ${message.guild.name}`);
-    c.overwritePermissions(message.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-    setInterval(function() {
-      c.setName(`Members Count : [ ${message.guild.members.size} ]`)
-    },1000);
-  });
-  }
 });
 
 client.login(process.env.TOKEN);
